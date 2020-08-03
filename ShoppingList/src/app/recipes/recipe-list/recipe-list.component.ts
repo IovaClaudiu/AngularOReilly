e0ff4262074ef959fd932368a92d5c22c7fd0578
Claudiu Iova <claudiu.iova@ibm.com>
2020-08-03 10:46:09 +0300

    Adding the Shopping List Project

---
+++ ShoppingList/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Recipe } from '../recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is just a test',
+      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is just a test',
+      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ ShoppingList/src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+export class Ingredient {
+  /**
+   * This will simply generate the public variables for this class.
+   * It's just a shortcut
+   * @param name The name of the ingredient
+   * @param amount The amount of ingredient
+   */
+  constructor(public name: string, public amount: number) {}
+}
