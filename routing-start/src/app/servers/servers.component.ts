f7de6c053ac0e5f9f0226c38fe2de29a9894a57b
Claudiu Iova <claudiu.iova@ibm.com>
2020-08-12 12:43:44 +0300

    Add routing to the project

--- routing-start/src/app/servers/servers.component.ts
+++ routing-start/src/app/servers/servers.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { ServersService } from "./servers.service";
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  selector: "app-servers",
+  templateUrl: "./servers.component.html",
+  styleUrls: ["./servers.component.css"],
 })
 export class ServersComponent implements OnInit {
-  public servers: {id: number, name: string, status: string}[] = [];
+  public servers: { id: number; name: string; status: string }[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  onReload() {
+    // By default, naviate is relative to route / no matter where you are
+    //this.router.navigate(["servers"], { relativeTo: this.route });
+    this.router.navigate(["servers"]);
+  }
 }
