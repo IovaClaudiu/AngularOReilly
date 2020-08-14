7f13c840e595940248dbe4707a7e8c9352da6662
Claudiu Iova <claudiu.iova@ibm.com>
2020-08-14 10:59:16 +0300

    Add observable project 14

---
+++ observable-project/src/app/home/home.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription, Observable } from "rxjs";
+import { map, filter } from "rxjs/operators";
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
+
+  constructor() {}
+
+  ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count == 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error("Count is greater 3!"));
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable
+      .pipe(
+        filter((data: number) => {
+          return data > 0;
+        }),
+        map((data: number) => {
+          return "Round: " + (data + 1);
+        })
+      )
+      .subscribe(
+        (data: string) => {
+          console.log(data);
+        },
+        (error: Error) => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          console.log("Completed!");
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
+}
