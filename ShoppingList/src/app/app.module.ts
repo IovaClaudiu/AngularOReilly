8f2403a631045d5a4886ddcc8dabd8076b986b3f
Claudiu Iova <claudiu.iova@ibm.com>
2020-09-14 12:04:54 +0300

    Add shopping list feature module

--- ShoppingList/src/app/app.module.ts
+++ ShoppingList/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { RecipesModule } from "./recipes/recipes.module";
 import { AlertComponent } from "./shared/alert/alert.component";
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
@@ -10,8 +11,6 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { DropdownDirective } from "./shared/dropdown.directive";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { AppRoutingModule } from "./app-routing.module";
@@ -21,8 +20,6 @@ import { RecipeService } from "./recipes/recipe.service";
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     DropdownDirective,
     AuthComponent,
     LoadingSpinnerComponent,
@@ -35,6 +32,7 @@ import { RecipeService } from "./recipes/recipe.service";
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
+    ShoppingListModule,
   ],
   providers: [
     ShoppingListService,
