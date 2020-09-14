47f8234bbf9ad001cca6fadf773a3d2c78922515
Claudiu Iova <claudiu.iova@ibm.com>
2020-09-14 12:31:08 +0300

    Add auth feature module to the project

--- ShoppingList/src/app/app.module.ts
+++ ShoppingList/src/app/app.module.ts
@@ -1,10 +1,9 @@
+import { AuthModule } from "./auth/auth.module";
 import { SharedModule } from "./shared/shared.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { RecipesModule } from "./recipes/recipes.module";
-import { AuthComponent } from "./auth/auth.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
@@ -13,17 +12,16 @@ import { AppRoutingModule } from "./app-routing.module";
 import { CoreModule } from "./core.module";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AuthComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
     SharedModule,
     CoreModule,
+    AuthModule,
   ],
   bootstrap: [AppComponent],
 })
